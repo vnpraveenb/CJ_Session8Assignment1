7c898a1949df3ed548dd06d43a69819bae64f720
Praveen Balijepalli <vnpraveenb@gmail.com>
2017-11-27 18:01:16 +0530

    Final Commit

--- src/filecopy/FileCopy.java
+++ src/filecopy/FileCopy.java
@@ -1,5 +1,5 @@
 /**
- * NOTE: Delete the contents of the outputTextFile.txt before running the program
+ *  
  */
 package filecopy;
 
@@ -7,7 +7,8 @@ import java.io.*; // importing java.io package
 
 /**
  * A class to copy the contents of a source file to a destination file. 
- * It also prints the contents of the source file check what contents are copied.
+ * It does this by creating a new file if destination file does not exist or overwriting it if it exists
+ * It also prints the contents of the source file contents that  are copied as well as the contents of the destination file
  * ram
  * @author Praveen
  * 
@@ -25,15 +26,22 @@ public class FileCopy {
 		FileReader outReader = null; 	// FileReader object to read from the destination file  
 
 		FileWriter writer = null;	// FileWriter object to write to destination file
+		
 
 		try {
 			// Gets the path of the file(source file = inputTextFile.txt) to read
 			inReader = new FileReader(System.getProperty("user.dir") + "/src/filecopy/inputTextFile.txt");
 
+			//Creates a new file object which has the following path
+			File newFile = new File(System.getProperty("user.dir") + "/src/filecopy/", "outputTextFile.txt");
+			
+			//Creates a new file which has the path of fileObject
+			newFile.createNewFile();
+			
 			// Gets the path of the file(destination file = outputTextFile.txt) to write
-			writer = new FileWriter(System.getProperty("user.dir")+ "/src/filecopy/outputTextFile.txt");
-
-			// Declares the variable which is used to read and print from the source file and to write to the destination file
+			writer = new FileWriter(newFile.getPath());
+			
+ 			// Declares the variable which is used to read and print from the source file and to write to the destination file
 			int inReadChar; 
 
 			System.out.println("Printing Source File Text:");
@@ -44,9 +52,10 @@ public class FileCopy {
 				writer.write((char)inReadChar); // Writes the character to the destination file
 
 			}
-			writer.close();
-			inReader.close();
-			System.out.println(" ");
+			writer.close(); //closes the destination file writer object
+			
+			
+ 			System.out.println(" ");
 			System.out.println("\nPrinting Destination File Text:");
 
 			// Get the path of the file(destination file = outputTextFile.txt) to read
