63c1bbddf197b073bcd43af5e895a6b9859d577b
Praveen Balijepalli <vnpraveenb@gmail.com>
2017-11-27 17:34:50 +0530

    Final Commit

--- src/filecopy/FileCopy.java
+++ src/filecopy/FileCopy.java
@@ -20,35 +20,58 @@ public class FileCopy {
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-		 FileReader reader = null; 	// FileReader object to read from a file  
-	     FileWriter writer = null;	// FileWriter object to write to a file
-  
-	     try {
-	    	  // Get the path of the file to read
-	    	  reader = new FileReader(System.getProperty("user.dir") + "/src/filecopy/inputTextFile.txt");
-	    	 
-	    	  // Gets the path of the file to write
-	    	  writer = new FileWriter(System.getProperty("user.dir")+ "/src/filecopy/outputTextFile.txt");
-	         
-	    	  // Declares the variable which is used to read from the source file and write to the destination file
-	          int readChar; 
-	          
-	          // While loop to read the file till the end of the file character is reached
-	          while ((readChar = reader.read()) != -1) {
-	            writer.write((char)readChar); // Writes the character to the destination file
-	            System.out.print((char)readChar); // Prints the character that is written to the destination file
-	         }
-	      }
-	      finally {
-	         if (reader != null) {
-	        	 reader.close(); //closes the reader object
-	         }
-	         if (writer != null) {
-	        	 writer.close(); //closes the writer object
-	         }
-      
-	      }
+
+		FileReader inReader = null; 	// FileReader object to read from source file  
+		FileReader outReader = null; 	// FileReader object to read from the destination file  
+
+		FileWriter writer = null;	// FileWriter object to write to destination file
+
+		try {
+			// Gets the path of the file(source file = inputTextFile.txt) to read
+			inReader = new FileReader(System.getProperty("user.dir") + "/src/filecopy/inputTextFile.txt");
+
+			// Gets the path of the file(destination file = outputTextFile.txt) to write
+			writer = new FileWriter(System.getProperty("user.dir")+ "/src/filecopy/outputTextFile.txt");
+
+			// Declares the variable which is used to read and print from the source file and to write to the destination file
+			int inReadChar; 
+
+			System.out.println("Printing Source File Text:");
+
+			// While loop to read the file till the end of the file character is reached
+			while ((inReadChar = inReader.read()) != -1) {
+				System.out.print((char)inReadChar); // Prints the character that is being written to the destination file
+				writer.write((char)inReadChar); // Writes the character to the destination file
+
+			}
+			writer.close();
+			inReader.close();
+			System.out.println(" ");
+			System.out.println("\nPrinting Destination File Text:");
+
+			// Get the path of the file(destination file = outputTextFile.txt) to read
+			outReader = new FileReader(System.getProperty("user.dir") + "/src/filecopy/outputTextFile.txt");
+
+			// Declares the variable which is used to read and print from the destination file
+			int outReadChar; 
+			
+			while ((outReadChar = outReader.read()) != -1) {
+				System.out.print((char)outReadChar); // Prints the character that is written to the destination file
+			}
+
+		}
+		finally {
+			if (inReader != null) {
+				inReader.close(); //closes the source file reader object
+			}
+			if (writer != null) {
+				writer.close(); //closes the destination file writer object
+			}
+			if (outReader != null) {
+				outReader.close(); //closes the destination file reader object
+			}
+
+		}
 	}
 
 }
